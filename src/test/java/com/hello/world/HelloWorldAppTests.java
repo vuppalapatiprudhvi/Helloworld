ba8acaeaae780772b30fdfec82c22abbed1b78db
vuppalapatiprudhvi <vuppalapatiprudhvi@gmail.com>
2022-05-16 14:42:31 +0530

    helloworld

---
+++ src/test/java/com/hello/world/HelloWorldAppTests.java
@@ -0,0 +1,13 @@
+package com.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
